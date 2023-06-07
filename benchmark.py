d272dc1ab17a26b98a98d87362d25c15bc6c15a7
Gopal <82232811+gopal-ag@users.noreply.github.com>
2023-06-07 11:35:37 +0530

    Add files via upload

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import cv2
 import time
 import sys
@@ -10,20 +8,20 @@ def benchmark(num_times):
     and return the execution time
     """
     start = time.clock_gettime(time.CLOCK_REALTIME)
-    # Load the cascade
+
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    # Read the input image
+
     img = cv2.imread('test.jpg')
 
-    # Convert into grayscale
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # overhead time just to set things up
+
     overhead_time = time.clock_gettime(time.CLOCK_REALTIME) - start
 
     start = time.clock_gettime(time.CLOCK_REALTIME)
-    # Detect faces
+
     for i in range(0,num_times): 
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
